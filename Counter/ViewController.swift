1806a905a4ee141f863f65b4741fbe3d11f0c626
Tim Bulokhov <timofeybrant@mail.ru>
2023-10-20 13:15:10 +0200

    Исправлены отступы, опечатки в форматировании даты, а также убраны лишние комментарии.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,39 +9,38 @@ import UIKit
 extension Date {
     var dateNow: String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM d, yyyy HH:mm:ss"
+        dateFormatter.dateFormat = "MMM d, YYYY HH:MM:SS"
         return dateFormatter.string(from: self)
     }
 }
 class ViewController: UIViewController {
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var refreshButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var changeHistory: UITextView!
-    
     private var counterNumber: Int = 0
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var refreshButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var changeHistory: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         changeHistory.isEditable = false
         changeHistory.isScrollEnabled = true
         
     }
-   
-    @IBAction func countPlus(_ sender: Any) {
+    
+    @IBAction private func countPlus(_ sender: Any) {
         counterNumber+=1
         counterLabel.text = "Значение счётчика: \(counterNumber)"
         changeHistory.text += "\n \(Date().dateNow) значение изменено на +1"
     }
     
-    @IBAction func countRefresh(_ sender: Any) {
+    @IBAction private func countRefresh(_ sender: Any) {
         counterNumber = 0
         counterLabel.text = "Значение счётчика: \(counterNumber)"
         changeHistory.text += "\n \(Date().dateNow) значение сброшено"
     }
     
-    @IBAction func countMinus(_ sender: Any) {
+    @IBAction private func countMinus(_ sender: Any) {
         if counterNumber>0 {
             counterNumber-=1
             counterLabel.text = "Значение счётчика: \(counterNumber)"
